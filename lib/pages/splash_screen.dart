6a155120c798dcb5c37b4b305a914b168add98c3
JuanM04 <me@juanm04.com>
2019-12-02 10:01:04 -0300

    Cloudflare pop-up

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,8 +1,10 @@
 import 'package:animu/components/updater.dart';
 import 'package:animu/utils/db.dart';
+import 'package:animu/utils/helpers.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_browser/flutter_web_browser.dart';
 import 'package:package_info/package_info.dart';
 import 'package:pub_semver/pub_semver.dart';
 
@@ -53,8 +55,35 @@ class _SplashScreenState extends State<SplashScreen> {
       );
   }
 
-  void getFavorites() async {
-    var favorites = await AnimeDatabase().getFavorites();
+  Future<void> checkCloudflare() async {
+    try {
+      await Dio().post('https://animeflv.net/api/animes/search');
+    } catch (e) {
+      if (e.response.statusCode == 503)
+        await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Error'),
+            content: Text(
+                'AnimeFLV es re ortvia, y por eso tenés que apretar este botón y esperar un par de segundos. Si sigue sin funcionar, tenés que esperar hasta un día :)'),
+            actions: <Widget>[
+              dialogButton(
+                'Este botón',
+                () =>
+                    FlutterWebBrowser.openWebPage(url: 'https://animeflv.net'),
+              ),
+            ],
+          ),
+        );
+    }
+  }
+
+  void initApp() async {
+    if (await isOnline() == false) return;
+    await checkUpdates();
+    await checkCloudflare();
+
+    final favorites = await AnimeDatabase().getFavorites();
     Navigator.pushReplacementNamed(
       context,
       '/',
@@ -62,15 +91,9 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
-  void runInOrder() async {
-    if (await isOnline() == false) return;
-    await checkUpdates();
-    getFavorites();
-  }
-
   @override
   void initState() {
-    runInOrder();
+    initApp();
     super.initState();
   }
 
