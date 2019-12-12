d95dacfd1899b7cddf5c4e3ee166a95819c24ba2
JuanM04 <me@juanm04.com>
2019-12-12 02:08:12 -0300

    Shitty "screen off" when casting

--- lib/pages/cast_player.dart
+++ lib/pages/cast_player.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:animu/components/previous_next.dart';
 import 'package:animu/utils/classes.dart';
 import 'package:animu/utils/helpers.dart';
 import 'package:animu/utils/notifiers.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:screen/screen.dart';
 
 class CastPlayer extends StatefulWidget {
   @override
@@ -14,9 +17,10 @@ class _CastPlayerState extends State<CastPlayer> {
   PlayerData data;
   SSHNotifier ssh;
   bool isPlaying = true;
+  bool screenLocked = false;
 
   void initPlayer() async {
-    final url = await getURLFromData(data);
+    final url = await getEpisodeURLFromData(data);
     ssh.client.writeToShell('omxplayer $url\n');
   }
 
@@ -29,6 +33,12 @@ class _CastPlayerState extends State<CastPlayer> {
   }
 
   @override
+  void dispose() {
+    Screen.keepOn(false);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     if (data == null) {
       data = ModalRoute.of(context).settings.arguments;
@@ -38,70 +48,91 @@ class _CastPlayerState extends State<CastPlayer> {
 
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          BackButton(),
-          SizedBox(height: 10),
-          Text(
-            data.anime.name,
-            style: TextStyle(
-              height: 1,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
-          ),
-          Text(
-            'Episodio ${data.currentEpisode.n}',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              GestureDetector(
-                onTap: () => ssh.client.writeToShell('^[[D'),
-                child: Icon(Icons.replay_30, size: 50),
-              ),
-              GestureDetector(
-                onTap: () {
-                  ssh.client.writeToShell('p');
-                  setState(() => isPlaying = !isPlaying);
-                },
-                child: Icon(
-                  isPlaying ? Icons.pause : Icons.play_arrow,
-                  size: 100,
+      body: screenLocked
+          ? GestureDetector(
+              onTap: () => setState(() {
+                Screen.keepOn(false);
+                screenLocked = false;
+              }),
+              child: Container(color: Colors.black),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    BackButton(),
+                    SizedBox(width: 30),
+                    GestureDetector(
+                      onTap: () => setState(() {
+                        Screen.keepOn(true);
+                        screenLocked = true;
+                      }),
+                      child: Icon(Icons.screen_lock_portrait, size: 25),
+                    ),
+                  ],
                 ),
-              ),
-              GestureDetector(
-                onTap: () => ssh.client.writeToShell('^[[C'),
-                child: Icon(Icons.forward_30, size: 50),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              PreviousNext(
-                type: PreviousNextType.previous,
-                data: data,
-                changeEpisode: changeEpisode,
-              ),
-              SizedBox(width: 20),
-              PreviousNext(
-                type: PreviousNextType.next,
-                data: data,
-                changeEpisode: changeEpisode,
-              ),
-            ],
-          ),
-        ],
-      ),
+                SizedBox(height: 20),
+                Text(
+                  data.anime.name,
+                  style: TextStyle(
+                    height: 1,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  'Episodio ${data.currentEpisode.n}',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () => ssh.client.writeToShell('^[[D'),
+                      child: Icon(Icons.replay_30, size: 50),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        ssh.client.writeToShell('p');
+                        setState(() => isPlaying = !isPlaying);
+                      },
+                      child: Icon(
+                        isPlaying ? Icons.pause : Icons.play_arrow,
+                        size: 100,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => ssh.client.writeToShell('^[[C'),
+                      child: Icon(Icons.forward_30, size: 50),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    PreviousNext(
+                      type: PreviousNextType.previous,
+                      data: data,
+                      changeEpisode: changeEpisode,
+                    ),
+                    SizedBox(width: 20),
+                    PreviousNext(
+                      type: PreviousNextType.next,
+                      data: data,
+                      changeEpisode: changeEpisode,
+                    ),
+                  ],
+                ),
+              ],
+            ),
     );
   }
 }
